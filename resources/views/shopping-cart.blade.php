462e3e9e19ba214845bdffe6fe51f70a947e7515
Jhonny Alejandro <jhonnycastano.studies@gmail.com>
2023-08-07 14:32:50 -0500

    Modulo de ventas
    Correccion de ruta

--- resources/views/shopping-cart.blade.php
+++ resources/views/shopping-cart.blade.php
@@ -79,7 +79,7 @@
             </div>
         </section>
         <div class="flex justify-end mt-8 px-4">
-            <a href="{{ route('checkout') }}" class="inline-flex justify-center w-full py-3 px-8 text-lg font-semibold text-white bg-indigo-600 rounded-md hover:bg-indigo-500 md:w-auto">
+            <a href="#" class="inline-flex justify-center w-full py-3 px-8 text-lg font-semibold text-white bg-indigo-600 rounded-md hover:bg-indigo-500 md:w-auto">
                 <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-7 h-7 mr-3">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"></path>
                 </svg>
